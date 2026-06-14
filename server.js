053e230f0a5357abb94f7862a09f4b80b9ed442c
joeagate95-blip <joeagate95@gmail.com>
2026-06-14 22:07:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -997,8 +997,20 @@ app.get('/api/admin-analytics', (req, res) => {
 });
 /* FRONTEND FALLBACK */
 
-app.get('*', (req, res) => {
+ app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
 /* START SERVER */
